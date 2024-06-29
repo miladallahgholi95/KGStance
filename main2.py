3091903870b436dcbd93b0be8802ee8bc2edebcd
miladallahgholi95 <miladallahgholi95@gmail.com>
2024-06-29 14:18:57 +0330

    kg stance develop

---
+++ main2.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Concatenate
+import numpy as np
+import pickle
+from utils import *
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import f1_score
+
+# read data
+with open('train_data.pkl', 'rb') as file:
+    train_data_json = pickle.load(file)
+
+with open('test_data.pkl', 'rb') as file:
+    test_data_json = pickle.load(file)
+
+# functions
+def embedding_validation(emb):
+    return isinstance(emb, list) and len(emb) == 768
+
+
+# model
+bert_embedding_dim = len(train_data_json[0]["embedding"])
+target_embedding_dim = len(train_data_json[0]["target_embedding"])
+
+bert_input = Input(shape=(bert_embedding_dim,), name='bert_input')
+target_input = Input(shape=(target_embedding_dim,), name='target_input')
+combined = Concatenate()([bert_input, target_input])
+hidden1 = Dense(256, activation='relu')(combined)
+hidden2 = Dense(128, activation='relu')(hidden1)
+hidden3 = Dense(64, activation='relu')(hidden2)
+output = Dense(3, activation='softmax')(hidden3)
+model = Model(inputs=[bert_input, target_input], outputs=output)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+# train data preparation
+bert_embeddings = np.array([d['embedding'] for d in train_data_json])
+target_embeddings = np.array([d['target_embedding'] for d in train_data_json])
+labels = np.array([d['Label'] for d in train_data_json])
+
+label_encoder = LabelEncoder()
+labels = label_encoder.fit_transform(labels)
+
+# fit model
+history = model.fit(
+    [bert_embeddings, target_embeddings],
+    labels,
+    epochs=100,
+    batch_size=8
+)
+
+# test data preparation
+test_labels, test_bert_embeddings, test_target_embeddings = [], [], []
+for row in test_data_json:
+    embedding_fields = ["exists_words_definition_embedding", "enrich_words_definition_embedding"]
+
+    bert_embeddings = []
+    for field in embedding_fields:
+        if embedding_validation(row[field]):
+            bert_embeddings.append(row[field])
+
+    if bert_embeddings:
+        test_bert_embeddings.append(np.array(average_vectors(bert_embeddings)))
+        test_target_embeddings.append(np.array(row['target_embedding']))
+        test_labels.append(row['Label'])
+
+test_labels_encoded = label_encoder.transform(test_labels)
+
+# predict model
+predictions = model.predict([np.array(test_bert_embeddings), np.array(test_target_embeddings)])
+predicted_classes = np.argmax(predictions, axis=1)
+
+# calculate F1-Measure
+f1 = f1_score(test_labels_encoded, predicted_classes, average='weighted')
+print(f"F1-Measure: {f1}")
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 def average_vectors(vectors):
+
+    if len(vectors) == 1:
+        return vectors[0]
+
     vectors_array = np.array(vectors)
     average_vector = np.mean(vectors_array, axis=0)
     return average_vector.tolist()
